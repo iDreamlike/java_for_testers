429a50b43f14b4a6bfaa4a9f18f5d86f88843474
Сергей Берсенев <bersenev@mail.ru>
2025-01-26 19:43:51 +0300

    Домашнее задание 4. Готово.

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Triangle(double a, double b, double c) {
 
     public Triangle {
@@ -18,5 +20,21 @@ public record Triangle(double a, double b, double c) {
     public double area() {
         double p = 0.5 * (this.a + this.b + this.c);
         return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.c) == 0
+        || Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.c) == 0 && Double.compare(this.c, triangle.a) == 0
+        || Double.compare(this.a, triangle.c) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.a) == 0
+        || Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.a) == 0 && Double.compare(this.c, triangle.c) == 0
+        || Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.c) == 0 && Double.compare(this.c, triangle.b) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -34,4 +34,18 @@ public class TriangleTests {
     void canCalculateAreaTriangle() {
         Assertions.assertEquals(10.825317547305483, new Triangle(5.0, 5.0, 5.0).area());
     }
+
+    @Test
+    void testEquality() {
+        var r1 = new Triangle(5.0, 4.0, 3.0);
+        var r2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(r1, r2);
+    }
+
+    @Test
+    void testEquality2() {
+        var r1 = new Triangle(3.0, 4.0, 5.0);
+        var r2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(r1, r2);
+    }
 }
