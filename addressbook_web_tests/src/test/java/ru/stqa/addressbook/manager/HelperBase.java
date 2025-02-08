160cc2e0c3934c8c4a028964d83a889cfed02c86
dreamlike <bersenev@mail.ru>
2025-02-08 15:25:02 +0300

    Домашнее задание 6. Готово

---
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/HelperBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.addressbook.manager;
+
+import org.openqa.selenium.By;
+
+public class HelperBase {
+    protected final ApplicationManager manager;
+
+    public HelperBase(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        manager.driver.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver.findElement(locator).clear();
+        manager.driver.findElement(locator).sendKeys(text);
+    }
+}
---
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.addressbook.tests;
+
+import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    void canModifyGroup() {
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+        app.groups().modifyGroup(new GroupData().withName("modified name"));
+//        app.groups().modifyGroup(new GroupData().withFooter("Modified Footer"));
+    }
+}
