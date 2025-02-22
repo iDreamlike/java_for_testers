36f30b6e3dbe044471ce09825ce4d93a6f8d9227
dreamlike <bersenev@mail.ru>
2025-02-23 01:42:31 +0300

    Часть 6.1. В процессе

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -14,6 +14,7 @@ public class ApplicationManager {
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private JdbcHelper jdbc;
     private Properties properties;
 
     public void init(String browser, Properties properties) {
@@ -52,6 +53,13 @@ public class ApplicationManager {
             contacts = new ContactHelper(this);
         }
         return contacts;
+    }
+
+    public JdbcHelper jdbc() {
+        if (jdbc == null) {
+            jdbc = new JdbcHelper(this);
+        }
+        return jdbc;
     }
 
     public void acceptAlert() {
