bb19d86e7b7b7b3f55f32260655df598af330b9e
dreamlike <bersenev@mail.ru>
2025-03-24 13:48:35 +0300

    Часть 7.2. Готово

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -3,18 +3,19 @@ package ru.stqa.geometry;
 import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
 
-import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Geometry {
     public static void main(String[] args) {
-        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
-//        for (Square square : squares) {
-//            Square.printSquareArea(square);
-//        }
-        squares.forEach(Square::printSquareArea);
+        Supplier<Square> randomSquare = () -> new Square(new Random().nextDouble(100.0));
+        var squares = Stream.generate(randomSquare).limit(5);
 
-//        Rectangle.printRectangleArea(3.0, 5.0);
-//        Rectangle.printRectangleArea(7.0, 9.0);
+//        Consumer<Square> print = square -> {
+//            Square.printSquareArea(square);
+//        };
+        squares.peek(Square::printSquareArea).forEach(Square::printSquareArea);
     }
 }
